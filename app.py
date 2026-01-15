1591bbfdb649a7f6c230c7b225071a3e88a5ca57
Majid Hadji <majidhadji@gmail.com>
2026-01-15 12:45:35 -0800

    deployment to Google cloud

--- app.py
+++ app.py
@@ -84,4 +84,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Cloud Run provides the PORT environment variable
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
